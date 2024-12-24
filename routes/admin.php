dd95f681ab5ce10bf3dde6061c5c70d6b1ed0593
bartukocakara <bartu.kocakara@tardigrad.com.tr>
2024-12-24 09:32:07 +0300

    init

--- routes/admin.php
+++ routes/admin.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\CommissionController;
+use App\Http\Controllers\CourtBusinessController;
+use App\Http\Controllers\CourtController;
 use Illuminate\Support\Facades\Route;
 
 Route::prefix('admin')->group(function () {
@@ -9,8 +13,10 @@ Route::prefix('admin')->group(function () {
     Route::post('/logout', [LoginController::class, 'logout'])->name('court_business.logout');
 
     Route::middleware('auth:court_business')->group(function () {
-        Route::get('/dashboard', function () {
-            return view('court-business.dashboard.index');
-        })->name('court_business.dashboard');
+        Route::get('/dashboard', [DashboardController::class, 'dashboard'])->name('admin.dashboard');
+        Route::resource('/courts', CourtController::class);
+        Route::resource('/court-businesses', CourtBusinessController::class);
+        Route::resource('/commissions', CommissionController::class);
+
     });
 });
