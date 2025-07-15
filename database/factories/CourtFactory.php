0178071b9931c468189ec5eec173e4142d96f332
bartukocakara <kocakarabartu@gmail.com>
2025-07-15 23:45:42 +0300

    request agreegates init

--- database/factories/CourtFactory.php
+++ database/factories/CourtFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use App\Models\CourtBusiness;
+use App\Models\Definition;
 use App\Models\District;
 use App\Models\SportType;
 use Illuminate\Support\Str;
@@ -24,7 +25,7 @@ class CourtFactory extends Factory
             'sport_type_id' => fake()->randomElement(SportType::all()->pluck('id')),
             'court_business_id' => fake()->randomElement(CourtBusiness::all()->pluck('id')),
             'title' => fake()->cityPrefix(). ' Sports Arena',
-           
+            'status' => fake()->randomElement(Definition::where('group_key', 'court_status')->pluck('value')),
         ];
     }
 }
